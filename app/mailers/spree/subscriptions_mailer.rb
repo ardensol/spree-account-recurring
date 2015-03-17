65f39633cca2093c5ff4dc4cce0d9438415a1ce3
carlos <csolorio05@gmail.com>
2015-03-16 20:17:06 -0700

    updated mailer

--- app/mailers/spree/subscriptions_mailer.rb
+++ app/mailers/spree/subscriptions_mailer.rb
@@ -1,13 +1,13 @@
 module Spree
   class SubscriptionsMailer < BaseMailer
-    def cancel_email(user, resend = false)
-      subject = "#{Spree::Store.current.name} #{Spree.t('order_mailer.confirm_email.subject')} ##{@order.number}"
-      mail(to: "admin@mydapperbox.com", subject: "Canceled Subscription: #{subject})"
+    def cancel_email(@email)
+      subject = "Cancelled: #{@user}"
+      mail(to: "admin@mydapperbox.com", subject: subject)
     end
 
-    def postpone_email(user, resend = false)
-      subject = 
-      mail(to: "admin@mydapperbox.com", subject: "Postponed for 3 Months: #{subject}")
+    def postpone_email(@email)
+      subject = "Postponed for 3 Months: #{@user}"
+      mail(to: "admin@mydapperbox.com", subject: subject)
     end
   end
 end
