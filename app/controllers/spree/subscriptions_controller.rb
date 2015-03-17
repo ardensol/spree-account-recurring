687cefa2364f405bc244480736b917c33b01d7ca
carlos <csolorio05@gmail.com>
2015-03-16 17:15:58 -0700

    testing cancellation

--- app/controllers/spree/subscriptions_controller.rb
+++ app/controllers/spree/subscriptions_controller.rb
@@ -14,7 +14,7 @@ module Spree
     end
 
     def cancellation
-      @subscription = spree_current_user.subscriptions.first
+      @subscription = spree_current_user.subscriptions.undeleted.first
       @plan = @subscription.plan
     end
 
