32b37455b0fd55a6c404f8b6d98d2d2f8f18061e
carlos <csolorio05@gmail.com>
2015-03-30 10:56:23 -0700

    updated if else staements for country and coupon

--- app/models/concerns/spree/recurring/stripe_recurring/api_handler/subscription_api_handler.rb
+++ app/models/concerns/spree/recurring/stripe_recurring/api_handler/subscription_api_handler.rb
@@ -6,14 +6,14 @@ module Spree
           def subscribe(subscription)
             raise_invalid_object_error(subscription, Spree::Subscription)
             customer = subscription.user.find_or_create_stripe_customer(subscription.card_token)
-            if subscription.country == "United States"
-              if subscription.coupon_code == ""
+            if subscription.country == "United States" && subscription.coupon_code == ""
                 customer.subscriptions.create(plan: subscription.api_plan_id)
-              else             
-                customer.subscriptions.create(plan: subscription.api_plan_id, coupon: subscription.coupon_code)
-              end
-            else
+            elsif subscription.country == "United States" && subscription.coupon_code != ""             
+              customer.subscriptions.create(plan: subscription.api_plan_id, coupon: subscription.coupon_code)
+            elsif subscription.country != "United States" && subscription.coupon_code == "" 
               customer.subscriptions.create(plan: subscription.intl_plan_id)
+            elsif subscription.country != "United States" && subscription.coupon_code != ""
+              customer.subscriptions.create(plan: subscription.intl_plan_id, coupon: subscription.coupon_code)
             end
           end
 
